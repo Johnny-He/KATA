652e1726ae006c613208e13ae964d0d58da70ed1
njkno <wazs4268@gmail.com>
2018-05-21 23:29:42 +0800

    refactor

--- KATAA/Function.cs
+++ KATAA/Function.cs
@@ -7,27 +7,6 @@ namespace KATAA
    
     public class Function
     {
-        public static int findCombinationsCount(int amount, int[] coins)
-        {
-            return findCombinationsCount2(amount, coins, 0);
-        }
-
-        public static int findCombinationsCount2(int amount, int[] coins, int checkFromIndex)
-        {
-            if (amount == 0)
-            {
-                return 1;
-            }
-            else if (amount < 0 || coins.Length == checkFromIndex)
-            {
-                return 0;
-            }
-            else
-            {
-                int withFirstCoin = findCombinationsCount2(amount - coins[checkFromIndex], coins, checkFromIndex);
-                int withoutFirstCoin = findCombinationsCount2(amount, coins, checkFromIndex + 1);
-                return withFirstCoin + withoutFirstCoin;
-            }
-        }
+        
     }
 }
---
+++ KATAA/KATA22_CountCombinations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KATAA
+{
+    class KATA22_CountCombinations
+    {
+        public static int findCombinationsCount(int amount, int[] coins)
+        {
+            return findCombinationsCount2(amount, coins, 0);
+        }
+
+        public static int findCombinationsCount2(int amount, int[] coins, int checkFromIndex)
+        {
+            if (amount == 0)
+            {
+                return 1;
+            }
+            else if (amount < 0 || coins.Length == checkFromIndex)
+            {
+                return 0;
+            }
+            else
+            {
+                int withFirstCoin = findCombinationsCount2(amount - coins[checkFromIndex], coins, checkFromIndex);
+                int withoutFirstCoin = findCombinationsCount2(amount, coins, checkFromIndex + 1);
+                return withFirstCoin + withoutFirstCoin;
+            }
+        }
+    }
+}
