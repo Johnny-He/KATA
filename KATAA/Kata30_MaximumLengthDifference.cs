49e1556a1e81b27b2e80556c2e18ea1ef6e430d0
Johnny <wazs4268@gmail.com>
2018-08-10 10:29:46 +0800

    'test'

---
+++ KATAA/Kata30_MaximumLengthDifference.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace KATAA
+{
+    public class Kata30_MaximumLengthDifference
+    {
+        public int Mxdiflg(string[] a1, string[] a2)
+        {
+            if (a1 == null || a2 == null)
+            {
+                return -1;
+            }
+            var max = 0;
+            for (int i = 0; i < a1.Length; i++)
+            {
+                for (int j = 0; j < a2.Length; j++)
+                {
+                    max = max < Math.Abs(a1[i].Length - a2[j].Length) ? Math.Abs(a1[i].Length - a2[j].Length) : max;
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ UnitTestProject1/MaximumLengthDifferenceTest.cs
@@ -0,0 +1,18 @@
+﻿using KATAA;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace KATAtest
+{
+    [TestClass]
+    public class MaximumLengthDifferenceTest
+    {
+        [TestMethod]
+        public void MxdiflgTest()
+        {
+            string[] s1 = new string[] { "hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz" };
+            string[] s2 = new string[] { "cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww" };
+            var actual = new Kata30_MaximumLengthDifference().Mxdiflg(s1,s2);
+            Assert.AreEqual(13, actual);
+        }
+
+    }
+}
