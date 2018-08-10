33bcf100c257109c1b492941cbd0902c9b67bae8
Johnny <wazs4268@gmail.com>
2018-08-10 10:18:52 +0800

    Kata30 done

--- KATAA/Program.cs
+++ KATAA/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using KATAA.KATA;
 
 namespace KATAA
 {
@@ -8,9 +6,7 @@ namespace KATAA
     {
         static void Main(string[] args)
         {
-           Console.WriteLine(new HighestRankNumberInAnArray().HighestRank(new int[] { 2,2,2,2,11,11,11,11,10,10,10,10,1,1,1,1}));
-          
-
+            Console.WriteLine(new Kata30_MaximumLengthDifference());
             Console.Read();
 
         }
