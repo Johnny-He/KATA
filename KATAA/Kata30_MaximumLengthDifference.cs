529fe568e53cd3fc5a38083b84ff7bc711d6847d
Johnny-He <wazs4268@gmail.com>
2018-08-13 00:26:59 +0800

    Kata30 Done &Test

---
+++ KATAA/Kata30_MaximumLengthDifference.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KATAA
+{
+    public class Kata30_MaximumLengthDifference
+    {
+        public int Mxdiflg(string[] a1, string[] a2)
+        {
+            if (a1.Length == 0 || a2.Length == 0)
+            { return -1; }
+
+            var max = 0;
+            for (int i = 0; i < a1.Length; i++)
+            {
+                for (int j = 0; j < a2.Length; j++)
+                {
+                    max = Math.Abs(a1[i].Length - a2[j].Length) > max ? Math.Abs(a1[i].Length - a2[j].Length) : max;
+                }
+            }
+
+            return max;
+        }
+    }
+}
